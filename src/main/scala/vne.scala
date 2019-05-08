a7fc267f491188a82a9af0d1f6540f4f9363eb2f
mahsa1372 <mhs_nrz@yahoo.com>
2019-05-08 14:55:15 +0200

    Greedy Funktion plus ungültige Mapping

--- src/main/scala/vne.scala
+++ src/main/scala/vne.scala
@@ -7,12 +7,13 @@ import org.apache.spark.graphx._
 import org.apache.spark.rdd._
 import org.apache.spark.graphx.lib
 //import org.apache.spark.graphx.lib.MyShortestPaths
+import scala.language.postfixOps
 import scala.collection.immutable
 import scala.collection.mutable.ListBuffer
 import scala.collection.immutable.ListMap
  
 object VneApp {
-
+/*
 	def vertexMappingRandom(sArray: Array[(Long, Int)], vArray: Array[(Long, Int)]) :Map[Int, Int] = {
                 val rnd = new scala.util.Random //random function
                 var svertexMap = new ListBuffer[Int]()
@@ -27,10 +28,33 @@ object VneApp {
 			}
                 (List.range(1, vArray.size+1) zip svertexMap.toList) toMap
 	}
+*/
+
+	def vertexMappingGreedy(sArray: Array[(Long, Int)], vArray: Array[(Long, Int)]) :Map[Int, Int] = {
+		var ssorted = sArray.sortBy(x => (-x._2, x._1))
+		var vsorted = vArray.sortBy(x => (-x._2, x._1))
+		var svertexMap = new ListBuffer[Int]()
+		var vvertexMap = new ListBuffer[Int]()
+			for (y <- 1 to vsorted.size ) {
+				if (vsorted(y-1)._2 <= ssorted(y-1)._2) { 
+					svertexMap += ssorted(y-1)._1.toInt 
+					vvertexMap += y
+				}
+				else {
+					svertexMap += 0
+					vvertexMap += y
+				}
+			}
+		(vvertexMap.toList zip svertexMap.toList) toMap
+	}
 
 	def updateCapacity(sArray: Array[(Long, Int)], vArray: Array[(Long, Int)], nmapping: Map[Int, Int]) :Unit = {
 		for (x <- 1 to vArray.size) {
-			sArray(nmapping(x)-1) = (nmapping(x).toLong, (sArray((nmapping(x))-1)._2)-(vArray(x-1)._2))
+			if ((sArray(nmapping(x)-1)._2)-(vArray(x-1)._2) < 0) {
+				sArray(nmapping(x)-1) = (nmapping(x).toLong, 0)
+			} else {
+				sArray(nmapping(x)-1) = (nmapping(x).toLong, (sArray((nmapping(x))-1)._2)-(vArray(x-1)._2))
+			}
 		}
 	}
 
@@ -41,34 +65,58 @@ object VneApp {
 		//sc.setLogLevel("ERROR") // ALL,DEBUG,ERROR,FATAL,TRACE,WARN,INFO,OFF
 
 		// SUBSTRATE NETWORK:
-		val svertexArray = Array( (1L, 3), (2L, 3), (3L, 4), (4L, 4), (5L, 5) )
+		val svertexArray = Array( (1L, 10), (2L, 10), (3L, 10), (4L, 10), (5L, 10) )
 		val svertexRDD: RDD[(Long, Int)] = sc.parallelize(svertexArray)
 		val sedgeArray = Array(Edge(1L,2L,(1,1000)), Edge(1L,5L,(1,1000)), Edge(2L,3L,(1,1000)), Edge(2L,5L,(1,1000)), Edge(3L,4L,(1,1000)), Edge(4L,5L,(1,1000)))
 		val sedgeRDD: RDD[Edge[(Int,Int)]] = sc.parallelize(sedgeArray)
 		val gs: Graph[Int, (Int, Int)] = Graph(svertexRDD, sedgeRDD)
 
-		// VIRTUAL NETWORK:
-		val vvertexArray = Array( (1L, 5), (2L, 4), (3L, 4))
-		val vvertexRDD: RDD[(VertexId, Int)] = sc.parallelize(vvertexArray)
-		val vedgeArray = Array(Edge(1L,2L,(100)), Edge(1L,3L,(80)), Edge(2L,3L,(40)))
-		val vedgeRDD: RDD[Edge[Int]] = sc.parallelize(vedgeArray)
-		val gv: Graph[Int, Int] = Graph(vvertexRDD, vedgeRDD)
+		// VIRTUAL NETWORK1:
+		val vvertexArray1 = Array( (1L, 9), (2L, 7), (3L, 7))
+		val vvertexRDD1: RDD[(VertexId, Int)] = sc.parallelize(vvertexArray1)
+		val vedgeArray1 = Array(Edge(1L,2L,(100)), Edge(1L,3L,(80)), Edge(2L,3L,(40)))
+		val vedgeRDD1: RDD[Edge[Int]] = sc.parallelize(vedgeArray1)
+		val gv1: Graph[Int, Int] = Graph(vvertexRDD1, vedgeRDD1)
+
+                // VIRTUAL NETWORK2:
+                val vvertexArray2 = Array( (1L, 2), (2L, 2))
+                val vvertexRDD2: RDD[(VertexId, Int)] = sc.parallelize(vvertexArray2)
+                val vedgeArray2 = Array(Edge(1L,2L,(100)), Edge(2L,1L,(40)))
+                val vedgeRDD2: RDD[Edge[Int]] = sc.parallelize(vedgeArray2)
+                val gv2: Graph[Int, Int] = Graph(vvertexRDD2, vedgeRDD2)
+
+                // VIRTUAL NETWORK3:
+                val vvertexArray3 = Array( (1L, 1), (2L, 1), (3L, 1), (4L, 1))
+                val vvertexRDD3: RDD[(VertexId, Int)] = sc.parallelize(vvertexArray3)
+                val vedgeArray3 = Array(Edge(1L,2L,(100)), Edge(2L,3L,(80)), Edge(3L,4L,(40)), Edge(4L,1L,(40)))
+                val vedgeRDD3: RDD[Edge[Int]] = sc.parallelize(vedgeArray3)
+                val gv3: Graph[Int, Int] = Graph(vvertexRDD3, vedgeRDD3)
 
 		gs.vertices.collect.foreach(println(_))
 		gs.edges.collect.foreach(println(_))
-		gv.vertices.collect.foreach(println(_))
-		gv.edges.collect.foreach(println(_))
+		gv1.vertices.collect.foreach(println(_))
+		gv1.edges.collect.foreach(println(_))
 
 		// RANDOM MAPPING:
-		val nodeMapping = vertexMappingRandom(svertexArray, vvertexArray)
-		println(s"Elements of nodeMapping = $nodeMapping")
+		//val nodeMapping = vertexMappingRandom(svertexArray, vvertexArray1)
+		//System.out.println(s"Elements of nodeMapping = $nodeMapping")
 
+		//GREEDY MAPPING:
+		val greedyMapping = vertexMappingGreedy(svertexArray,vvertexArray1)
+	
 		// UPDATE CAPACITY:
-		updateCapacity(svertexArray, vvertexArray, nodeMapping)
+		updateCapacity(svertexArray, vvertexArray1, nodeMapping)
 		svertexArray
 
 		// TEST MORE VNs:
-
+		// Greedy mapping VN2, update capacity
+                //var greedyMapping = vertexMappingGreedy(svertexArray, vvertexArray2)
+                //updateCapacity(svertexArray, vvertexArray2, greedyMapping)
+		//svertexArray
+		// Greedy mapping VN3, update capacity
+                //var greedyMapping = vertexMappingGreedy(svertexArray, vvertexArray3)
+                //updateCapacity(svertexArray, vvertexArray3, greedyMapping)
+                //svertexArray
 
 		// Example 2: Kürzeste Pfade, extern definiert: MyShortestPath.scala (basierend auf existierendes GraphX->ShortestPaths)
 
@@ -77,8 +125,8 @@ object VneApp {
 
 
 		// MyShortestPath
-		val sp = MyShortestPaths.run(gs,List(3)) // 1,2,3... all considered dest.
-		sp.vertices.collect.foreach(println(_)) // Array((4,Map(4 -> 0)), (1,Map(4 -> 3)), (5,Map()), (2,Map(4 -> 2)), (3,Map(4 -> 1))) // case: 4 -> from 4: cost 0, from 1: cost 3,...
+	//	val sp = MyShortestPaths.run(gs,List(3)) // 1,2,3... all considered dest.
+	//	sp.vertices.collect.foreach(println(_)) // Array((4,Map(4 -> 0)), (1,Map(4 -> 3)), (5,Map()), (2,Map(4 -> 2)), (3,Map(4 -> 1))) // case: 4 -> from 4: cost 0, from 1: cost 3,...
 	}
 }
 
