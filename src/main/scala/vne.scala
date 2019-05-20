6bd948cd8a331dd5b855c35a6608dc67c4506589
mahsa1372 <mhs_nrz@yahoo.com>
2019-05-20 11:51:39 +0200

    Write Output to File

--- src/main/scala/vne.scala
+++ src/main/scala/vne.scala
@@ -6,6 +6,7 @@ import org.apache.spark.{SparkContext, SparkConf}
 import org.apache.spark.graphx._
 import org.apache.spark.rdd._
 import org.apache.spark.graphx.lib
+import java.io._
 import scala.language.postfixOps
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.immutable
@@ -56,6 +57,11 @@ object VneApp {
 		case Nil => (0, List())
 	}
 
+	def printToFile(f: java.io.File)(op: java.io.PrintWriter => Unit) {
+		val p = new java.io.PrintWriter(f)
+		try { op(p) } finally { p.close() }
+	}
+
 	def main(args: Array[String]): Unit = {
 		val conf = new SparkConf().setAppName("VNE")
 		val sc = new SparkContext(conf)
@@ -63,7 +69,7 @@ object VneApp {
 		//sc.setLogLevel("ERROR") // ALL,DEBUG,ERROR,FATAL,TRACE,WARN,INFO,OFF
 
 		// SUBSTRATE NETWORK:
-		val svertexArray = Array( (1L, ("1", 10)), (2L, ("2", 10)), (3L, ("3", 10)), (4L, ("4", 10)), (5L, ("5", 10)) )
+		val svertexArray = Array( (1L, ("1", 5)), (2L, ("2", 6)), (3L, ("3", 8)), (4L, ("4", 9)), (5L, ("5", 10)) )
 		val svertexRDD: RDD[(VertexId, (String, Int))] = sc.parallelize(svertexArray)
 		val sedgeArray = Array(Edge(1L,2L,(1,1000)), Edge(1L,5L,(5,1000)), Edge(2L,3L,(1,1000)), Edge(2L,5L,(4,1000)), Edge(3L,4L,(1,1000)), Edge(4L,5L,(1,1000)), Edge(5L,3L,(2,1000)))
 		val sedgeRDD: RDD[Edge[(Int,Int)]] = sc.parallelize(sedgeArray)
@@ -113,10 +119,47 @@ object VneApp {
                 //updateCapacity(svertexArray, vvertexArray3, greedyMapping)
                 //svertexArray
 
-		// ShortestPath
+		// SHORTEST PATH
 		val look = sedgeArray.map{ case Edge(srcId, dstId, (attr1,attr2)) => Array(srcId, attr1, dstId)}.groupBy(edge => edge(0)).map{ case (key, look) => (key.toLong, look.map{case Array(src, attr, dst) => (attr.toDouble, dst) }.toSet.toList)}
 		val res = Dijkstra[Long](look, List((0, List(1L))), 5L, Set())
 		println(res)
+
+		// WRITE TO A FILE
+		printToFile(new File("/home/test")){ p =>
+                        p.println("================================================")
+                        p.println("===============Substrate Network:===============")
+                        p.println("================================================")
+			gs.vertices.collect.foreach(p.println)
+	                gs.edges.collect.foreach(p.println)
+                        p.println("================================================")
+                        p.println("===============Virtual Network1:================")
+                        p.println("================================================")
+			gv1.vertices.collect.foreach(p.println)
+	                gv1.edges.collect.foreach(p.println)
+                        p.println("================================================")
+                        p.println("===============Virtual Network2:================")
+                        p.println("================================================")
+                        gv2.vertices.collect.foreach(p.println)
+                        gv2.edges.collect.foreach(p.println)
+                        p.println("================================================")
+                        p.println("===============Virtual Network3:================")
+                        p.println("================================================")
+                        gv3.vertices.collect.foreach(p.println)
+                        gv3.edges.collect.foreach(p.println)
+			p.println("================================================")
+			p.println("================Greedy Mapping:=================")
+                        p.println("================================================")
+			greedyMapping.foreach(p.println)
+                        p.println("================================================")
+			p.println("=========update capacity after mapping:=========")
+                        p.println("================================================")
+			svertexArray.foreach(p.println)
+                        p.println("================================================")
+                        p.println("===========Shortest Path from 1 to 5============")
+                        p.println("================================================")
+			p.println(res)
+		}
+
 	}
 }
 
