0dfc18b1fbebd3862a8045b2a84f9fefb0d83265
Mahsa Noroozi <mhs_nrz@nodec.spark-test.spork-join.filab.uni-hannover.de>
2019-10-22 02:26:50 -0600

    To push

--- src/main/scala/org/apache/spark/graphx/optimization/mip/LP/SolveRandom.scala
+++ src/main/scala/org/apache/spark/graphx/optimization/mip/LP/SolveRandom.scala
@@ -29,7 +29,7 @@ object SolveRandom extends Serializable {
 
 		// --------------------Define the substrate network using nodes and edges------------------------------
 //		val r = scala.util.Random.nextInt(30)
-		val r = 20
+		val r = 40
 		val s = scala.util.Random
 		var svertexArray = Array.ofDim [(Long, (String, Int))] (r)
 		for (i <- 1 to r) {
@@ -58,7 +58,7 @@ object SolveRandom extends Serializable {
 
                 // --------------------Define the virtual network using nodes and edges--------------------------------
 //		val rr = scala.util.Random.nextInt(r)
-		val rr = 8
+		val rr = 5
 		var vvertexArray = Array.ofDim [(Long, (String, Int))] (rr)
 		for (i <- 1 to rr) {
                         vvertexArray(i-1) = (i.toLong, (i.toString, s.nextInt(10)))
