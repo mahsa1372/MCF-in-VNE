2ad3488771a06314b19f8ee8e91bff8e2fed4ced
mahsa1372 <mhs_nrz@yahoo.com>
2019-11-04 13:24:13 +0100

    New changes with solve function

--- src/main/scala/org/apache/spark/graphx/optimization/mip/LP/SolveMCF3.scala
+++ src/main/scala/org/apache/spark/graphx/optimization/mip/LP/SolveMCF3.scala
@@ -30,6 +30,7 @@ import org.apache.spark.mllib.optimization.mip.lp.VectorSpace._
 class SolveMCF3 (gs: Graph[(String, Int), (Int, Int)], gv: Graph[(String, Int), (Int, Int)], Source: Tuple2[Int, Int], Destination: Tuple2[Int, Int], @transient sc: SparkContext) extends Serializable {
 
                 // --------------------Define matrix of constraints and vector of costs--------------------------------
+		private val numPartitions : Int = 512				// number of partitions
                 private val m = gv.vertices.collect.size                        // number of virtual nodes
                 private val n = gs.vertices.collect.size                        // number of substrate nodes
                 private val mm = m*(m-1)                                        // number of virtual links
@@ -134,13 +135,13 @@ class SolveMCF3 (gs: Graph[(String, Int), (Int, Int)], gv: Graph[(String, Int), 
                         }
                 }
 
-		private val A: DMatrix = sc.parallelize(a.transpose(s => s)).map(Vectors.dense(_)).zipWithIndex
+		private val A: DMatrix = sc.parallelize(a.transpose(s => s),numPartitions).map(Vectors.dense(_)).zipWithIndex
 //		private val C: Vector = new DenseVector(c)
-		private val C: DVector = sc.parallelize(c).glom.map(new DenseVector(_))
+		private val C: DVector = sc.parallelize(c,numPartitions).glom.map(new DenseVector(_))
 		private val B: DenseVector = new DenseVector(b)
 
                 // --------------------Solve the problem using simplex algorithm---------------------------------------
-		val lp = new SimplexReduction(A, B, C, sc=sc)
+		val lp = new SimplexReduction(A, B, C, numPartitions, sc=sc)
 		def SolveMCFinLP () :Array[Double] ={
 			val x = lp.solve()
 			x
